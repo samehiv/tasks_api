d057b1d5066f693e75c51cf85aedc861658406d7
Sameh AbdelZaher <sameh.iv@gmail.com>
2019-01-26 17:15:39 +0200

    replace any empty param with null

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const tasksRouter = require('./routes/tasks');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use((err, req, res, next) => {
+    for(key in req.body) {
+        if(req.body[key].trim() == '') {
+            req.body[key] = null
+        }
+    }
+
+    for(key in req.query) {
+        if(req.query[key].trim() == '') {
+            req.query[key] = null
+        }
+    }
+})
+
 app.use('/tasks', tasksRouter)
 
 
@@ -14,15 +28,17 @@ app.use((req, res) => {
     res.json({
         code: 404,
         status: 'error',
-        message: 'not found'
+        message: 'not found',
     })
 })
 
 app.use((err, req, res, next) => {
+    
     res.json({
         code: err.code,
         status: 'error',
-        message: err.message
+        message: err.message,
+        errors: err.errors ? err.errors : []
     })
 })
 
